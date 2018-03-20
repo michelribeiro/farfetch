2f3038197a0259b0990751ad28850e2a32e1abde
Michel Ribeiro <michelribeiro2004@gmail.com>
2018-03-20 14:52:06 -0300

    projeto estruturado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './pages';
+import './scss/index.scss';
+import App from './pages/index.js';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
